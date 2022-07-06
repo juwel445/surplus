44edc8d9af3245e8e517470b34e47d45d5da2a2c
juwel445 <57298051+juwel445@users.noreply.github.com>
2022-07-06 14:15:14 +0600

    uploading start

---
+++ assets/js/main.js
@@ -0,0 +1,178 @@
+// global object
+const GLOB = {};
+
+// testimonial splider
+GLOB.testimonialSplider = function () {
+  const container = document.getElementById("testimonialSplide");
+  const options = {
+    type: "loop",
+    arrows: false,
+    pagination: false,
+    perPage: 3,
+    autoplay: true,
+    gap: 25,
+    mediaQuery: "min",
+    destroy: "completely",
+    focus: "center",
+    breakpoints: {
+      576: {
+        destroy: false,
+        perPage: 1,
+      },
+      992: {
+        perPage: 3,
+      },
+    },
+  };
+
+  // initialized splider
+  var splide = new Splide(container, options);
+
+  // mount it to the DOM
+  splide.mount();
+};
+
+// strategy splider
+GLOB.strategySplider = function () {
+  const container = document.getElementById("strategySplide");
+  const options = {
+    type: "loop",
+    arrows: false,
+    pagination: false,
+    autoplay: true,
+    gap: 10,
+    mediaQuery: "min",
+    destroy: "completely",
+    breakpoints: {
+      576: {
+        destroy: false,
+        perPage: 3,
+      },
+      768: {
+        perPage: 4,
+      },
+      992: {
+        perPage: 5,
+      },
+      1200: {
+        perPage: 7,
+      },
+    },
+  };
+
+  // initialized splider
+  var splide = new Splide(container, options);
+
+  // mount it to the DOM
+  splide.mount();
+};
+
+// partners splider
+GLOB.partnersSplider = function () {
+  const container = document.getElementById("partnersSplide");
+  const options = {
+    type: "loop",
+    arrows: false,
+    pagination: false,
+    autoplay: true,
+    gap: 10,
+    mediaQuery: "min",
+    destroy: "completely",
+    breakpoints: {
+      576: {
+        destroy: false,
+        perPage: 3,
+      },
+      768: {
+        perPage: 4,
+      },
+      992: {
+        perPage: 5,
+      },
+      1200: {
+        perPage: 7,
+      },
+    },
+  };
+
+  // initialized splider
+  var splide = new Splide(container, options);
+
+  // mount it to the DOM
+  splide.mount();
+};
+
+// stats calculate
+GLOB.statCalculator = function () {
+  const statInput = document.getElementById("statAmount");
+
+  function calculate(ele) {
+    let slectPrice = ele.parentNode.parentNode.children[0].children[1];
+    ele.style.background = `linear-gradient(to right, #403489 0%, #403489 ${
+      ((ele.value - ele.min) / (ele.max - ele.min)) * 100
+    }%, #DEE2E6 ${
+      ((ele.value - ele.min) / (ele.max - ele.min)) * 100
+    }%, #DEE2E6 100%)`;
+    // Result in percentage
+    ele.parentNode.children[0].innerHTML = formatter
+      .format(ele.value)
+      .replace("$", "");
+    calculateProjectionYears(ele.value * 1);
+  }
+  /*
+
+X = D(1 + r)^y
+X = Projection Year Amount 
+D = Investment Amount 
+r = period rate (30%)
+y = number of years
+
+*/
+  function calculateProjection(amt, ra, yr) {
+    return amt * Math.pow(1 + ra / 100, yr);
+  }
+
+  function calculateProjectionYears(amt) {
+    let years = [1, 2, 3, 4, 5, 10, 20, 30];
+    let eachYrVal = [];
+    years.forEach((y, ind) => {
+      let val = calculateProjection(amt, 30, y);
+      eachYrVal[ind] = val;
+    });
+    eachYrVal.forEach((vl, ind) => {
+      document.getElementById("py" + years[ind]).innerHTML = formatter
+        .format(vl)
+        .replace("$", "");
+    });
+  }
+
+  let formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+
+    // These options are needed to round to whole numbers if that's what you want.
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
+
+  statInput.addEventListener("input", function () {
+    calculate(this);
+  });
+};
+
+// document on load
+document.addEventListener("DOMContentLoaded", function () {
+  GLOB.testimonialSplider();
+  GLOB.strategySplider();
+  GLOB.partnersSplider();
+  GLOB.statCalculator();
+});
+
+// document on scroll
+
+// document on resize
+window.addEventListener("resize", function () {
+  GLOB.testimonialSplider();
+  GLOB.strategySplider();
+  GLOB.partnersSplider();
+});
